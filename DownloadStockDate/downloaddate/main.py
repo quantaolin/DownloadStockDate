e332367aa38dfc5e140b14cc7541bb9825e4da97
linqt <linqt@linqt-PC>
2018-07-05 01:28:45 +0800

    1

--- DownloadStockDate/downloaddate/main.py
+++ DownloadStockDate/downloaddate/main.py
@@ -1,5 +1,12 @@
 '''
-Created on 2018Äê7ÔÂ5ÈÕ
+Created on 2018-07-05 01:27
 
 @author: linqt
 '''
+from pymongo import MongoClient
+
+conn = MongoClient('127.0.0.1', 27017)
+db = conn.mydb
+my_set = db.test_set
+my_set.insert({"name":"zhangsan","age":18})
+print(my_set.find_one({"name":"zhangsan"}))
